d592df6ea30fcbe1b6278a9fe3097c5445e15750
Andrei <andrei@Andreis-iMac.lan>
2020-10-09 21:50:58 -0400

    Add variablex.rb - Work with variables

---
+++ Methods/variables.rb
@@ -0,0 +1,23 @@
+number = 4
+greeting = "hello"
+
+puts number # 4
+puts greeting # hello
+puts number + 2 # 6
+puts 12 - number # 8
+
+# change variables
+number = 6
+greeting = "Hi"
+
+
+puts number # 6
+puts greeting # Hi
+puts number + 2 # 8
+puts 12 - number # 6
+
+
+# Valid variable names - same as method names.
+# All lower case
+# Numbers are legal but rarely used
+# Snake case - separate words with underscores.

a9c81d96d7a37266c3f3620867b2e3abc27597cd
Andrei <andrei@Andreis-iMac.lan>
2020-10-09 21:08:49 -0400

    Defining methods

---
+++ Methods/defining_methods.rb
@@ -0,0 +1,22 @@
+# Wait methods
+def wait
+    puts "Hacking the Pentagon"
+    sleep 3
+    puts "Waiting..."
+    count_to_three
+    puts "Done!"
+end
+
+# Count methods counts 3 times
+def count_to_three
+    sleep 2
+    puts 3 # Print 3 in Terminal
+    sleep 1 # Waiting 1 second
+    puts 2
+    sleep 1
+    puts 1
+    sleep 2
+end
+
+# Check our methods
+wait

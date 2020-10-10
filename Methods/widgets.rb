7c0d798fc3dcd4567b7f07eca9de6c7caad6da8c
Andrei <andrei@Andreis-iMac.lan>
2020-10-10 18:41:13 -0400

    Add a widgets.rb

---
+++ Methods/widgets.rb
@@ -0,0 +1,12 @@
+def ask(question)
+    print question + " "
+    gets.chomp # cut last /n 
+end
+
+puts "Welcome to the widget store!"
+answer = ask("How many widgets are you ordering?")
+number = answer.to_i 
+puts "For #{number} widgets, your total is: $#{number * 10}"
+
+
+

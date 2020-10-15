64ee980b604be285ac37f57b5d49b12f599a68b1
Andrei Komolov <andreikomolov@Andreis-iMac.lan>
2020-10-15 10:56:51 -0400

    Add a print separator def

--- ruby_collections/shopingList.rb
+++ ruby_collections/shopingList.rb
@@ -1,3 +1,4 @@
+# Create new list return hash with name of list and Array with items 
 def create_list
     puts "What is a list name? "
     name = gets.chomp
@@ -7,6 +8,7 @@ def create_list
 end
 
 
+# Adding new item to the list (return hash with one item)
 def add_new_items_to_list
     puts "What is a item called "
     item_name = gets.chomp
@@ -18,22 +20,27 @@ def add_new_items_to_list
     return hash
 end
 
+# Output info on screen from any list that you put as argument
 def print_list(list)
     puts "List name: #{list['name']}"
-    puts "-----"
+    print_separator()
     
     list['items'].each do |item|
-        puts "Item: " + item['name']
-        puts "Quantity: " + item['quantity'].to_s
-        puts "-----"
+        puts "\t\t\tItem: " + item['name'] + "\t\t\tQuantity: " + item['quantity'].to_s
     end
+    ``
+    print_separator()
 end
 
+# Sepatator for list items
+def print_separator(character = "-")
+    puts character * 80
+end
+
+#Main program circle
 list = create_list()
 puts list
-puts ""
 
-list['items'].push(add_new_items_to_list())
 list['items'].push(add_new_items_to_list())
 list['items'].push(add_new_items_to_list())
 

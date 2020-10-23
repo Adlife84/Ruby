4f69732b191aaccbfaa08f85552ea93c3dc7c4ee
Andrei Komolov <andreikomolov@Andreis-iMac.lan>
2020-10-23 19:52:40 -0400

    Build a monster class

--- Blocks/monster_class.rb
+++ Blocks/monster_class.rb
@@ -4,7 +4,9 @@ class Monster
     def initialize(name)
         @name = name
         @actions = {
-            screams: 0
+            screams: 0,
+            run: 0,
+            hide: 0
         }
     end
 
@@ -13,10 +15,34 @@ class Monster
         yield
     end
 
+    def run(&block)
+        @actions[:run] += 1
+        print "#{name} runs! "
+        yield
+    end
+
+    def hide(&block)
+        @actions[:hide] += 1
+        print "#{name} hides"
+        yield
+    end
+
     def scream(&block)
         @actions[:screams] += 1
         print "#{name} screams!"
         yield
+    end
+
+    def print_scoreboard
+        puts "-" * 30
+        puts "#{name} scoreboard"
+        puts "-" * 30
+        puts "- Scream: #{@actions[:screams]}"
+        puts "-" * 30
+        puts "- Hide: #{@actions[:hide]}"
+        puts "-" * 30
+        puts "- Run: #{@actions[:run]}"
+        puts "-" * 30 
     end
 end
 
@@ -26,4 +52,18 @@ monster.say { puts "Welcome to my home."}
 monster.scream do 
     puts "BOO!!!"
 end
+
+monster.scream do 
+    puts "BOO!!!"
+end
+
+monster.run do
+    puts "Going to get you!"
+end
+
+monster.hide do
+    puts "Running away and hiding!"
+end
+
+monster.print_scoreboard
 

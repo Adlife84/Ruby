176581899e10f96d330081326ea63c2d43989faa
Andrei Komolov <andreikomolov@Andreis-iMac.lan>
2020-10-24 13:58:41 -0400

    Add address_class.rb

--- Build_an_adress_book/contact_class.rb
+++ Build_an_adress_book/contact_class.rb
@@ -1,5 +1,25 @@
+require "./phone_number.rb"
+
 class Contact
     attr_writer :first_name, :middle_name, :last_name
+    attr_reader :phone_numbers
+
+    def initialize 
+        @phone_numbers = []
+    end
+
+    def add_phone_number(kind, number)
+        phone_number = PhoneNumber.new
+        phone_number.kind = kind
+        phone_number.number = number
+        phone_numbers.push(phone_number) 
+    end
+
+    def print_phone_numbers
+        puts "--------Phone numbers----------"
+        phone_numbers.each { |number| puts number }
+        puts "-------------------------------"
+    end
 
     def first_name
         @first_name
@@ -62,6 +82,11 @@ andrey.first_name = "Andrey"
 andrey.middle_name = "Igorevich"
 andrey.last_name = "Komolov"
 
+andrey.add_phone_number("Phone", "911-123-45-67")
+andrey.add_phone_number("Phone", "912-321-54-76")
+puts andrey.phone_numbers
+puts andrey.inspect
+andrey.print_phone_numbers
 
 puts andrey.full_name
 puts andrey.to_s

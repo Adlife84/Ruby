c9126c5c1ef6a9a66d266dc8cd12d5d3c2b85d9f
Andrei Komolov <andreikomolov@Andreis-iMac.lan>
2020-10-25 22:12:11 -0400

    Add find by adress

--- Build_an_adress_book/address_book_class.rb
+++ Build_an_adress_book/address_book_class.rb
@@ -28,6 +28,42 @@ class AddressBook
         end
     end
 
+    def find_by_phone_number(number)
+        results = []
+        search = number.gsub("-", "") # gsub method change first argument for second one
+        contacts.each do |contact|
+            contact.phone_numbers.each do |phone_number|
+                if phone_number.number.gsub("-", "").include?(search)
+                    results.push(contact) unless results.include?(contact)
+                end
+            end
+        end
+        print_results("Phone search results (#{search}", results)
+    end
+
+    def find_by_address(query)
+        results = []
+        search = query.downcase
+        contacts.each do |contact|
+            contact.addresses.each do |address|
+                if address.to_s('long').downcase.include?(search)
+                    results.push(contact) unless results.include?(contact)
+                end
+            end
+        end
+        print_results("Addresss search results: #{search}", results)
+    end
+
+    def print_results(search, results)
+        puts "#{search}"
+        results.each do |contact|
+            puts contact.to_s("full_name")
+            contact.print_phone_numbers
+            contact.print_addresses
+            puts "\n" 
+        end
+    end
+
     def print_contact_list
         puts "Contact List"
         contacts.each do |contact|
@@ -58,19 +94,23 @@ jose.middle_name = "Marcus"
 jose.last_name = "Dacascos"
 
 jose.add_phone_number("Phone", "911-987-65-43")
-jose.add_phone_number("Phone", "912-312-22-44")
+jose.add_phone_number("Phone", "912-412-22-44")
 
 jose.add_address("Office", "2201 Main St.", "", "Texas", "TX", "27564")
 
 addressbook.contacts.push(jose)
-
-
-
-
 addressbook.print_contact_list
 
 puts "-" * 100
 # We call addressbook .find_by_name method with "Andrey" 
 addressbook.find_by_name("Jose")
 addressbook.find_by_name("Andrey")
+
+puts "-" * 100
+# Call find_by_phone_number method with "911"
+addressbook.find_by_phone_number("123")
+
+puts "-" * 100
+# Caaled find_by_address method to find "2201" address
+addressbook.find_by_address("2201")
 

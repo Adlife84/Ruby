21473dc56097a3220e74d83deb996ae13aaf0b45
Andrei Komolov <andreykomolov@gmail.com>
2020-11-05 17:46:54 -0500

    Add create new page POST method

--- Sinatra/Wiki_pages/wiki.rb
+++ Sinatra/Wiki_pages/wiki.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "uri"
 
 set :bind, "0.0.0.0"
 
@@ -6,6 +7,13 @@ def page_content(title)
     File.read("pages/#{title}.txt")
 rescue Errno::ENOENT
     return nil
+end
+
+# Save title, content from argument to file /pages/title.txt 
+def save_content(title, content)
+    File.open("pages/#{title}.txt", "w") do |file| # title as argument
+        file.print(content) # content as a string puts on the file
+    end
 end
 
 get ("/test") do
@@ -17,9 +25,24 @@ get ("/") do
     erb :welcome # load from template
 end
 
+get ("/new") do
+    erb :new # load from template
+end
+
+
+post ("/create") do
+    "Hello from '/create' route!"
+    #params.inspect  - returns {"title"=>"asdas", "content"=>"asdadsasd"}
+    save_content(params["title"], params["content"])
+    redirect URI.escape("/#{params["title"]}")
+end
+
+
+
+
+# This route have to be in the end becouse it can take any parameters from URL
 get "/:title" do
     @title = params[:title]
     @content = page_content(@title)
     erb :show # load template from show.erb
 end
-
